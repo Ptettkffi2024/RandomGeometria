edd2d4df69461676c0c578a14ecd9a41d8d55c51
dorottyakoczor <koczor.dodo@gmail.com>
2025-01-10 11:37:10 +0100

    random geometria generálás fő metódusa

--- Randomgeometria/MyGeometries/RandomGeometriaGenerator.cs
+++ Randomgeometria/MyGeometries/RandomGeometriaGenerator.cs
@@ -19,6 +19,29 @@ namespace Randomgeometria
             lineGenerator = new LineGenerator(random);
             polygonGenerator = new PolygonGenerator(random);
         }
-        
+
+         // Ez a fő metódus, amely egy véletlenszerű geometriát generál a kért típus alapján.
+        public NetTopologySuite.Geometries.Geometry GenerateGeometry(string geometryType)
+        {
+            // Egy Dictionary-t hozok létre, amely a geometriatípusokat a generálási metódusaikhoz rendeli
+            var geometryCreators = new Dictionary<string, Func<NetTopologySuite.Geometries.Geometry>>
+            {
+                { "point", () => pointGenerator.CreatePoint() }, // pont generálása
+                { "line", () => lineGenerator.CreateLineString() }, // vonal generálása
+                { "polygon", () => polygonGenerator.CreatePolygon() } //polygon generálása
+            };
+
+            // Geometria típus normalizálása
+            string key = geometryType.Trim().ToLower();
+
+            // A megfelelő generátor kiválasztása a Dictionary-ből
+            if (geometryCreators.TryGetValue(key, out var createGeometry))
+            {
+                return createGeometry();
+            }
+
+            // Ha a kért geometriatípus nem támogatott, akkor ez az üzenet jelenik meg
+            throw new NotSupportedException($"The geometry type '{geometryType}' is not supported.");
+        }
     }
 }
