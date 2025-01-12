f39da28c0695c3ae30133f7b20be771bf1c95d99
dorottyakoczor <koczor.dodo@gmail.com>
2025-01-12 18:34:34 +0100

    Random Linestring létrehozásának metódusa

--- Randomgeometria/MyGeometries/LineGenerator.cs
+++ Randomgeometria/MyGeometries/LineGenerator.cs
@@ -16,5 +16,33 @@ namespace Randomgeometria
             geometryFactory = new GeometryFactory();
             this.random = new Random();
         }
+
+         // Itt hozom létre a LineSrting objektumot véletlenszerű koordinátákból
+        public LineString CreateLineString()
+        {
+            int numPoints = random.Next(2, 10); // Véletlenszerűen kiválasztja, hogy hány pontból álljon a vonal, 2 és 10 között
+            var coordinates = new Coordinate[numPoints]; // A pontokat egy koordináta tömbben tárolja
+            
+            // A tömb feltöltése véletlenszerű koordinátákkal
+            for (int i = 0; i < numPoints; i++)
+            {
+                coordinates[i] = CreateCoordinate();
+            }
+            return geometryFactory.CreateLineString(coordinates);
+        }
+
+        // Létrehoz egy véletlenszerű koordinátát x és y értékkel (én -100 és +100 közé tettem, hogy random ebből az intervallumból választhasson)
+        private Coordinate CreateCoordinate()
+        {
+            double x = GenerateRandomValue(-100, 100);
+            double y = GenerateRandomValue(-100, 100);
+            return new Coordinate(x, y);
+        }
+
+        // Véletlenszerű számot generál a megadott min és max érték között
+        private double GenerateRandomValue(double min, double max)
+        {
+            return min + random.NextDouble() * (max - min);
+        }
     }
 }
